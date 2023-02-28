97fd058d3b8a0e0fa47723bcb05c3564b46d3710
ferika0702 <ferikaputra07@gmail.com>
2023-02-28 11:38:05 +0700

    penambahan alert

--- app/Views/mahasiswa/index.php
+++ app/Views/mahasiswa/index.php
@@ -12,7 +12,19 @@
   <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/dashboard/">
 
 
+<!--  -->
+<!-- JavaScript -->
+<script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
 
+<!-- CSS -->
+<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
+<!-- Default theme -->
+<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/default.min.css"/>
+<!-- Semantic UI theme -->
+<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/semantic.min.css"/>
+<!-- Bootstrap theme -->
+<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/bootstrap.min.css"/>
+<!--  -->
   <!-- Bootstrap core CSS -->
   <link href="/css/bootstrap.min.css" rel="stylesheet">
 
